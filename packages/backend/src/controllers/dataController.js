405062550cef20b232e9920a837c1997913db4a7
codemonkey-sagar <fmtsagar@gmail.com>
2024-05-21 08:16:57 -0400

    merkletree_implementation:testing

--- packages/backend/src/controllers/dataController.js
+++ packages/backend/src/controllers/dataController.js
@@ -1,32 +1,20 @@
-const axios = require('axios');
-const encryptionService = require('../services/encryptionService');
-const databaseService = require('../services/databaseService');
-const logger = require('../utils/logger');
+const axios = require("axios");
+const hashingService = require("../services/hashingService");
+const databaseService = require("../services/databaseService");
+const logger = require("../utils/logger");
+const {
+  buildMerkleTree,
+  getMerkleRoot,
+  generateMerkleProof,
+} = require("../services/merkleTreeService");
 
 exports.uploadData = async (req, res) => {
   try {
-    const { hash, logs } = req.body;
     console.log(req.body);
+    const { hash, logs } = req.body;
 
-    // Parse logs if it is a JSON string
-    const parsedLogs = typeof logs === 'string' ? JSON.parse(logs) : logs;
-
-    if (!parsedLogs || !hash) {
-      return res.status(400).send({
-        error: 'Error in parsed logs or hash',
-      });
-    }
-
-    // Calculate hash of the JSON data
-    const calculatedHash = encryptionService.calculateHash(parsedLogs);
-    logger.log(`Calculated Hash: ${calculatedHash}`);
-
-    // Compare Hashes
-    if (calculatedHash !== hash) {
-      return res.status(400).send({
-        error: "Data integrity check failed. Hash doesn't match",
-      });
-    }
+    // Converting byte array from frontend into hash
+    const calculatedHash = hashingService.calculateHash(hash);
 
     // Check if hash exists in database
     const hashExists = await databaseService.hashExists(calculatedHash);
@@ -35,18 +23,30 @@ exports.uploadData = async (req, res) => {
       logger.log(`Hash exists in database: ${calculatedHash}`);
 
       // Respond to frontend with hash match message
-      res.status(200).send({
-        message: 'Hash exists in database. Logs sent to `/received_logs`',
+      return res.status(200).send({
+        message: "Hash exists in database. Logs sent to `/received_logs`",
       });
+    } else {
+      logger.log(`Hash does not exist in database: ${calculatedHash}`);
 
-      // Send parsedLogs to `/received_logs`
+      // Hash doesn't exist, insert into database
+      await databaseService.insertLeafHash(calculatedHash);
+
+      // Build the Merkle tree
+      await buildMerkleTree();
+
+      // Get the root hash and proof
+      const rootHash = await getMerkleRoot();
+      const proof = await generateMerkleProof(calculatedHash);
+
+      // Send the response to `/received_logs`
       try {
         const response = await axios.post(
-          'http://localhost:3000/received_logs',
+          "http://localhost:3000/received_logs",
           {
-            logs: parsedLogs,
-            hash: hash,
-            calculatedHash: calculatedHash,
+            rootHash: rootHash,
+            hash: calculatedHash,
+            proof: proof,
           }
         );
         logger.log(`Logs sent to /received_logs: ${response.status}`);
@@ -55,21 +55,15 @@ exports.uploadData = async (req, res) => {
           `Error sending logs to /received_logs: ${sendError.message}`
         );
       }
-    } else {
-      logger.log(`Hash does not exist in database: ${calculatedHash}`);
-
-      // Hash doesn't exist, insert into database
-      await databaseService.insertHash(calculatedHash);
 
       return res.status(200).send({
-        message:
-          'Data received and hash verified successfully. Hash inserted into database.',
+        message: "Data received and hash inserted into Merkle Tree.",
       });
     }
   } catch (error) {
     logger.error(`Error processing uploadData: ${error.message}`);
     return res.status(500).send({
-      error: 'An error occurred while processing the data',
+      error: "An error occurred while processing the data",
     });
   }
 };
--- packages/backend/src/services/databaseService.js
+++ packages/backend/src/services/databaseService.js
@@ -1,39 +1,61 @@
 const { Pool } = require("pg");
+const dotenv = require("dotenv");
 
-// Create a new pool instance with our database configuration
+dotenv.config();
+
 const pool = new Pool({
-  user: "postgres",
-  host: "hackfs-logfile-instance.c9kk0qeoiarl.us-east-2.rds.amazonaws.com",
-  database: "hackfs_logfile",
-  password: "postgres",
-  port: 5432,
+  user: process.env.DATABASE_USER,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: process.env.DATABASE_SSL_REJECT_UNAUTHORIZED === "true",
   },
 });
 
-// Function to check if a hash exists in the database
 exports.hashExists = async (hash) => {
-  const client = await pool.connect();
-
-  try {
-    // CREATE INDEX idx_hash_hash ON hashes USING hash (hash): Hash index is created for optimizing (only search is required)
-    const result = await client.query("SELECT 1 FROM hashes WHERE hash = $1", [
-      hash,
-    ]);
-    return result.rowCount > 0;
-  } finally {
-    client.release();
-  }
+  const res = await pool.query(
+    "SELECT 1 FROM merkle_nodes WHERE node_hash = $1",
+    [hash]
+  );
+  return res.rowCount > 0;
 };
 
-// Function to insert a new hash into the database
-exports.insertHash = async (hash) => {
-  const client = await pool.connect();
+exports.insertLeafHash = async (hash) => {
+  await pool.query(
+    "INSERT INTO merkle_nodes (node_hash, level) VALUES ($1, 0)",
+    [hash]
+  );
+};
 
-  try {
-    await client.query("INSERT INTO hashes (hash) VALUES ($1)", [hash]);
-  } finally {
-    client.release();
-  }
+exports.getNodesAtLevel = async (level) => {
+  const res = await pool.query(
+    "SELECT id, node_hash FROM merkle_nodes WHERE level = $1",
+    [level]
+  );
+  return res.rows;
+};
+
+exports.insertNode = async (hash, leftChildId, rightChildId, level) => {
+  const res = await pool.query(
+    "INSERT INTO merkle_nodes (node_hash, left_child, right_child, level) VALUES ($1, $2, $3, $4) RETURNING id",
+    [hash, leftChildId, rightChildId, level]
+  );
+  return res.rows[0].id;
+};
+
+exports.updateNodeParent = async (id, parentId) => {
+  await pool.query("UPDATE merkle_nodes SET parent_id = $1 WHERE id = $2", [
+    parentId,
+    id,
+  ]);
+};
+
+exports.getNodeParent = async (id) => {
+  const res = await pool.query(
+    "SELECT * FROM merkle_nodes WHERE id = (SELECT parent_id FROM merkle_nodes WHERE id = $1)",
+    [id]
+  );
+  return res.rows[0];
 };
