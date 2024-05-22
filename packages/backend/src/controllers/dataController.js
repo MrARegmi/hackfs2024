8438b89abfe989bd6d530f68bc3243dfca337462
codemonkey-sagar <fmtsagar@gmail.com>
2024-05-22 13:40:43 -0400

    merkle tree implementation

--- packages/backend/src/controllers/dataController.js
+++ packages/backend/src/controllers/dataController.js
@@ -10,59 +10,67 @@ const {
 
 exports.uploadData = async (req, res) => {
   try {
-    console.log(req.body);
-    const { hash, logs } = req.body;
+    const { hash: hashByteArray, logs } = req.body;
 
-    // Converting byte array from frontend into hash
-    const calculatedHash = hashingService.calculateHash(hash);
+    const backendByteArray = hashingService.calculateByteArray(logs);
+    const calculatedHash = hashingService.calculateHash(hashByteArray);
 
-    // Check if hash exists in database
+    const areByteArrayEqual = Buffer.from(hashByteArray).equals(
+      Buffer.from(backendByteArray)
+    );
+    if (!areByteArrayEqual) {
+      return res.status(400).send({
+        success: false,
+        error: "Byte array mismatch",
+      });
+    }
+
     const hashExists = await databaseService.hashExists(calculatedHash);
-
     if (hashExists) {
-      logger.log(`Hash exists in database: ${calculatedHash}`);
-
-      // Respond to frontend with hash match message
       return res.status(200).send({
-        message: "Hash exists in database. Logs sent to `/received_logs`",
+        success: true,
+        message: "Hash already exists in Merkle Tree",
       });
     } else {
-      logger.log(`Hash does not exist in database: ${calculatedHash}`);
-
-      // Hash doesn't exist, insert into database
       await databaseService.insertLeafHash(calculatedHash);
-
-      // Build the Merkle tree
       await buildMerkleTree();
-
-      // Get the root hash and proof
       const rootHash = await getMerkleRoot();
-      const proof = await generateMerkleProof(calculatedHash);
+      const merkleProof = await generateMerkleProof(calculatedHash);
 
-      // Send the response to `/received_logs`
       try {
         const response = await axios.post(
-          "http://localhost:3000/received_logs",
+          "http://localhost:8080/received_logs",
           {
+            success: true,
+            message: "Data committed successfully.",
+            dataHash: calculatedHash,
             rootHash: rootHash,
-            hash: calculatedHash,
-            proof: proof,
+            path: merkleProof.path.join("."),
+            merkleProof: {
+              siblings: merkleProof.siblings,
+              parentHashes: merkleProof.parentHashes,
+            },
           }
         );
-        logger.log(`Logs sent to /received_logs: ${response.status}`);
+
+        return res.status(201).send({
+          success: true,
+          message: "Data received and hash inserted into Merkle Tree.",
+        });
       } catch (sendError) {
         logger.error(
           `Error sending logs to /received_logs: ${sendError.message}`
         );
+        return res.status(500).send({
+          success: false,
+          error: "Failed to send logs to `/received_logs`",
+        });
       }
-
-      return res.status(200).send({
-        message: "Data received and hash inserted into Merkle Tree.",
-      });
     }
   } catch (error) {
     logger.error(`Error processing uploadData: ${error.message}`);
     return res.status(500).send({
+      success: false,
       error: "An error occurred while processing the data",
     });
   }
